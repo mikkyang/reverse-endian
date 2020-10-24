65c3c9bfd5bebeaf44891a2684fd2535e5aeed17
Michael Yang <mikkyangg@gmail.com>
2020-10-24 16:06:01 +0900

    Implement body of reverse endian for struct

--- reverse_endian_derive/src/lib.rs
+++ reverse_endian_derive/src/lib.rs
@@ -2,7 +2,10 @@ extern crate proc_macro2;
 extern crate quote;
 extern crate syn;
 
-use syn::{parse_quote, GenericParam, Generics};
+use proc_macro2::TokenStream;
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{parse_quote, Data, Fields, GenericParam, Generics, Ident, Index};
 
 fn add_trait_bounds(mut generics: Generics) -> Generics {
     for param in &mut generics.params {
@@ -14,4 +17,37 @@ fn add_trait_bounds(mut generics: Generics) -> Generics {
     }
 
     generics
+}
+
+fn reverse_endian(name: &Ident, data: &Data) -> TokenStream {
+    match *data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => {
+                let recurse = fields.named.iter().map(|f| {
+                    let name = &f.ident;
+                    quote_spanned! {f.span() =>
+                        #name: reverse_endian::ReverseEndian::reverse_endian(self.#name)
+                    }
+                });
+                quote! {
+                    #name {
+                        #(#recurse,)*
+                    }
+                }
+            }
+            Fields::Unnamed(ref fields) => {
+                let recurse = fields.unnamed.iter().enumerate().map(|(i, f)| {
+                    let index = Index::from(i);
+                    quote_spanned! {f.span()=>
+                        reverse_endian::ReverseEndian::reverse_endian(&self.#index)
+                    }
+                });
+                quote! {
+                    #name(#(#recurse, )*)
+                }
+            }
+            Fields::Unit => quote!( #name{} ),
+        },
+        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+    }
 }
