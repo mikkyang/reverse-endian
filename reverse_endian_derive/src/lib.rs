353658fd7ac1952776382d33538ca9c6c92f4cc9
Michael Yang <mikkyangg@gmail.com>
2020-10-24 16:10:38 +0900

    Implement derive

--- reverse_endian_derive/src/lib.rs
+++ reverse_endian_derive/src/lib.rs
@@ -1,10 +1,35 @@
+extern crate proc_macro;
 extern crate proc_macro2;
 extern crate quote;
 extern crate syn;
 
 use quote::{quote, quote_spanned};
-use syn::spanned::Spanned;
-use syn::{parse_quote, Data, Fields, GenericParam, Generics, Ident, Index};
+use syn::{
+    parse_macro_input, parse_quote, spanned::Spanned, Data, DeriveInput, Fields, GenericParam,
+    Generics, Ident, Index,
+};
+
+#[proc_macro_derive(ReverseEndian)]
+pub fn derive_reverse_endian(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let generics = add_trait_bounds(input.generics);
+    let (impl_generics, type_generics, where_clause) = generics.split_for_impl();
+
+    let reversed = reverse_endian(&name, &input.data);
+
+    let output = quote! {
+        impl #impl_generics reverse_endian::ReverseEndian for #name #type_generics #where_clause {
+            fn reverse_endian(self) -> Self {
+                #reversed
+            }
+        }
+    };
+
+    proc_macro::TokenStream::from(output)
+}
 
 fn add_trait_bounds(mut generics: Generics) -> Generics {
     for param in &mut generics.params {
