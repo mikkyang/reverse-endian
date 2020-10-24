0fe85e41c1c9a06edd3c371eeb3ac9bc0ce9d662
Michael Yang <mikkyangg@gmail.com>
2020-10-24 15:58:39 +0900

    Add trait bound function

--- reverse_endian_derive/src/lib.rs
+++ reverse_endian_derive/src/lib.rs
@@ -1,3 +1,17 @@
 extern crate proc;
 extern crate quote;
 extern crate syn;
+
+use syn::{parse_quote, GenericParam, Generics};
+
+fn add_trait_bounds(mut generics: Generics) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(ref mut type_param) = *param {
+            type_param
+                .bounds
+                .push(parse_quote!(reverse_endian::ReverseEndian));
+        }
+    }
+
+    generics
+}
